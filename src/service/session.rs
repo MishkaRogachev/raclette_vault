c5b4e09422581593d52e97e5e7491d3095cbea27
MishkaRogachev <mishkarogachev@gmail.com>
2024-09-29 19:51:08 +0200

    feat: use OS-specific db path for release build

--- src/service/session.rs
+++ src/service/session.rs
@@ -58,7 +58,17 @@ impl Session {
 fn db_path() -> std::io::Result<std::path::PathBuf> {
     if cfg!(test) {
         Ok(std::env::temp_dir())
+    } else if cfg!(debug_assertions) {
+        Ok(std::env::current_dir()?)
     } else {
-        std::env::current_dir()
+        if let Some(proj_dirs) = directories::ProjectDirs::from(
+            "com", "raclettevault", "RacletteVault") {
+            Ok(proj_dirs.data_dir().to_path_buf())
+        } else {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "Unable to determine a proper data directory",
+            ))
+        }
     }
 }
